d28732caceb4a691f29a257c80d2eed289ccd80d
Ykulkarni-ops <kulkarniyash333@gmail.com>
2022-03-20 14:51:29 -0400

    Task 3 [check obstacle for circle and ellipse]

--- code/Astar_class_code.py
+++ code/Astar_class_code.py
@@ -24,3 +24,15 @@ class Astar(object):
     # check if the clearance for the point robot is valid    
     def validClearance(self, currRow, currCol):
         return (currRow >= (1 + self.radius + self.clearance) and currRow <= (self.numRows - self.radius - self.clearance) and currCol >= (1 + self.radius + self.clearance) and currCol <= (self.numCols - self.radius - self.clearance))
+
+
+    #check the obstacle space and plot it 
+    def obstacle(self, row, col):
+        sum = self.clearance + self.radius
+        sqrt_rc = 1.4142 * sum
+
+        # check circle
+        dist1 = ((row - 150) * (row - 150) + (col - 225) * (col - 225)) - ((25 + sum) * (25 + sum))
+        
+        # check eclipse
+        dist2 = ((((row - 100) * (row - 100)) / ((20 + sum) * (20 + sum))) + (((col - 150) * (col - 150)) / ((40 + sum) * (40 + sum)))) - 1
