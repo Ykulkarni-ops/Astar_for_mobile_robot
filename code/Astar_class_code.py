fb69d281ec1479a5bc22d59819b2704ed5aa910a
Ykulkarni-ops <kulkarniyash333@gmail.com>
2022-03-20 15:34:10 -0400

    Task 4 [Other obstacles created]

--- code/Astar_class_code.py
+++ code/Astar_class_code.py
@@ -36,3 +36,70 @@ class Astar(object):
         
         # check eclipse
         dist2 = ((((row - 100) * (row - 100)) / ((20 + sum) * (20 + sum))) + (((col - 150) * (col - 150)) / ((40 + sum) * (40 + sum)))) - 1
+
+
+         # check triangles
+        (x1, y1) = (120 - (2.62 * sum_of_c_and_r), 20 - (1.205 * sum_of_c_and_r))
+        (x2, y2) = (150 - sqrt_of_c_and_r, 50)
+        (x3, y3) = (185 + sum_of_c_and_r, 25 - (sum_of_c_and_r * 0.9247))
+        first = ((col - y1) * (x2 - x1)) - ((y2 - y1) * (row - x1))
+        second = ((col - y2) * (x3 - x2)) - ((y3 - y2) * (row - x2))
+        third = ((col - y3) * (x1 - x3)) - ((y1 - y3) * (row - x3))
+        dist3 = 1
+        if(first <= 0 and second <= 0 and third <= 0):
+            dist3 = 0
+            
+        (x1, y1) = (150 - sqrt_of_c_and_r, 50)
+        (x2, y2) = (185 + sum_of_c_and_r, 25 - (sum_of_c_and_r * 0.9247))
+        (x3, y3) = (185 + sum_of_c_and_r, 75 + (sum_of_c_and_r * 0.5148))
+        first = ((col - y1) * (x2 - x1)) - ((y2 - y1) * (row - x1))
+        second = ((col - y2) * (x3 - x2)) - ((y3 - y2) * (row - x2))
+        third = ((col - y3) * (x1 - x3)) - ((y1 - y3) * (row - x3))
+        dist4 = 1
+        if(first >= 0 and second >= 0 and third >= 0):
+            dist4 = 0
+        
+        # check rhombus
+        (x1, y1) = (10 - sqrt_of_c_and_r, 225)
+        (x2, y2) = (25, 200 - sqrt_of_c_and_r)
+        (x3, y3) = (40 + sqrt_of_c_and_r, 225)
+        (x4, y4) = (25, 250 + sqrt_of_c_and_r)
+        first = ((col - y1) * (x2 - x1)) - ((y2 - y1) * (row - x1))
+        second = ((col - y2) * (x3 - x2)) - ((y3 - y2) * (row - x2))
+        third = ((col - y3) * (x4 - x3)) - ((y4 - y3) * (row - x3))
+        fourth = ((col - y4) * (x1 - x4)) - ((y1 - y4) * (row - x4))
+        dist5 = 1
+        dist6 = 1
+        if(first >= 0 and second >= 0 and third >= 0 and fourth >= 0):
+            dist5 = 0
+            dist6 = 0
+        
+        # check square
+        (x1, y1) = (150 - sqrt_of_c_and_r, 50)
+        (x2, y2) = (120 - sqrt_of_c_and_r, 75)
+        (x3, y3) = (150, 100 + sqrt_of_c_and_r)
+        (x4, y4) = (185 + sum_of_c_and_r, 75 + (sum_of_c_and_r * 0.5148))
+        first = ((col - y1) * (x2 - x1)) - ((y2 - y1) * (row - x1))
+        second = ((col - y2) * (x3 - x2)) - ((y3 - y2) * (row - x2))
+        third = ((col - y3) * (x4 - x3)) - ((y4 - y3) * (row - x3))
+        fourth = ((col - y4) * (x1 - x4)) - ((y1 - y4) * (row - x4))
+        dist7 = 1
+        dist8 = 1
+        if(first <= 0 and second <= 0 and third <= 0 and fourth <= 0):
+            dist7 = 0
+            dist8 = 0
+        
+        # check rod
+        first = ((col - 95) * (8.66 + sqrt_of_c_and_r)) - ((5 + sqrt_of_c_and_r) * (row - 30 + sqrt_of_c_and_r))
+        second = ((col - 95) * (37.5 + sqrt_of_c_and_r)) - ((-64.95 - sqrt_of_c_and_r) * (row - 30 + sqrt_of_c_and_r))
+        third = ((col - 30.05 + sqrt_of_c_and_r) * (8.65 + sqrt_of_c_and_r)) - ((5.45 + sqrt_of_c_and_r) * (row - 67.5))
+        fourth = ((col - 35.5) * (-37.49 - sqrt_of_c_and_r)) - ((64.5 + sqrt_of_c_and_r) * (row - 76.15 - sqrt_of_c_and_r))
+        dist9 = 1
+        dist10 = 1
+        if(first <= 0 and second >= 0 and third >= 0 and fourth >= 0):
+            dist9 = 0
+            dist10 = 0
+        
+        if(dist1 <= 0 or dist2 <= 0 or dist3 == 0 or dist4 == 0 or dist5 == 0 or dist6 == 0 or dist7 == 0 or dist8 == 0 or dist9 == 0 or dist10 == 0):
+            return True
+        return False
